f45457b389c5819111fcf0c83ebfa6f52869105e
LaurentiuDeac <laurr_extrem12@yahoo.com>
2022-07-14 20:36:32 +0300

    primitiv si nonprimitiv

---
+++ src/test/java/ValueReference/Tabla.java
@@ -0,0 +1,18 @@
+package ValueReference;
+
+public class Tabla {
+
+    public String denumire;
+    public String material;
+
+    public Tabla(String denumire, String material) {
+        this.denumire = denumire;
+        this.material = material;
+    }
+
+    public void  info(){
+        System.out.println(denumire+" "+material);
+
+    }
+
+}
